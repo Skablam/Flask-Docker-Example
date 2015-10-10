f85fbfd2725d0744ad2aac69fac4a1cf1449f11a
Skablam <stainlesssteelmatt@gmail.com>
2015-10-10 21:52:41 +0100

    added test and modified Dockerfile to add working directory

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
+import os
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    blue="Blue"
+    return 'Hello True {0}!'.format(blue)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True, port=int(os.getenv('PORT', 5000)))
---
+++ tests/test_example.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_one(self):
+        assert 3 == 3
